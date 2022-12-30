d7c511d0ab317bad2fb742662c65f71aa5f91f3b
Pooja123-sharma <117913332+Pooja123-sharma@users.noreply.github.com>
2022-12-30 15:57:51 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-letarr = [1,2,3,4,5,6,7,8,9];
-//arr =arr.filter((v) => v % 2 ! == 0);
+let arr = [1,2,3,4,5,6,7,8,9];
+//arr = arr.filter((v) => v % 2 ! == 0);
 let total = 0;
 for (let i = 0; i < arr.length; i++) {
   if (arr[i] % 2 === 0){
